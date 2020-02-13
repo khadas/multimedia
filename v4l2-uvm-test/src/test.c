1b23760dd608dcf2ac769c0e188cd3159218bd1a
Song Zhao <song.zhao@amlogic.com>
2020-02-13 10:58:57 -0800

    UT: add v4l2 decoder + uvm dmabuf test [1/1]
    PD#SWPL-20176
    
    Problem:
    Need a unit test for UVM+V4L2 decoder
    
    Solution:
    *) Use FFMPEG as demuxer, feed framed data into V4L2 decoder.
    *) Fetch buffer from DRM-GEM, use DMABUF mode of V4L2 decoder.
    *) Use libdrm for render, take 2 fence delay into consideration.
    *) Use alpha 0 frame on OSD to show video plane.
    
    Verify:
    U212 G12A platform.
    //No DW
    v4l2-uvm-test -f $FILE_PATH -p 35 -d 0
    //DW 1:1
    v4l2-uvm-test -f $FILE_PATH -p 35 -d 1
    //DW 1:2
    v4l2-uvm-test -f $FILE_PATH -p 35 -d 4
    //DW only
    v4l2-uvm-test -f $FILE_PATH -p 35 -d 16
    //Adaptive stream (mpeg2/h265)
    
    Change-Id: I890e8ec5c6b88e60227656b19d2d7af3a04c76c1
    Signed-off-by: Song Zhao <song.zhao@amlogic.com>

---
+++ v4l2-uvm-test/src/test.c
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ *
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
+ *
+ * Description:
+ */
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "drm.h"
+#include "demux.h"
+#include "v4l2-dec.h"
+
+static char* media_file;
+static sem_t wait_for_end;
+int global_plane_id;
+int g_dw_mode = 16;
+int ffmpeg_log = 0;
+
+static void usage(int argc, char **argv)
+{
+        printf(  "Usage: %s [options]\n\n"
+                 "Version 0.1\n"
+                 "Options:\n"
+                 "-d | --double_write  double write mode.\n"
+                 "                     0: no DW compressed only\n"
+                 "                     1: DW with 1:1 ratio\n"
+                 "                     2: DW with 1:4 down sample\n"
+                 "                     4: DW with 1:2 down sample\n"
+                 "                     16: DW only\n"
+                 "-f | --file name     media file\n"
+                 "-h | --help          Print this message\n"
+                 "-p | --plane=id      select display plane. 26[pri] 28[overlay 1] 30[overlay 2] 32[video]\n"
+                 "-l | --log           enable more ffmpeg demux log.\n"
+                 "",
+                 argv[0]);
+}
+
+static const char short_options[] = "d:hf:p:l";
+
+static const struct option
+long_options[] = {
+        { "double_write", required_argument, NULL, 'd' },
+        { "file", required_argument, NULL, 'f' },
+        { "help",   no_argument,       NULL, 'h' },
+        { "plane",  required_argument, NULL, 'p' },
+        { "log",  no_argument, NULL, 'l' },
+        { 0, 0, 0, 0 }
+};
+
+void decode_finish()
+{
+    sem_post(&wait_for_end);
+}
+
+int start_decoder(struct dmx_v_data *v_data)
+{
+    int ret;
+    ret = v4l2_dec_init(v_data->type, decode_finish);
+    if (ret) {
+        printf("FATAL: start_decoder error:%d\n",ret);
+        exit(1);
+    }
+    return 0;
+}
+
+static int parse_para(int argc, char *argv[])
+{
+    for (;;) {
+        int idx;
+        int c;
+
+        c = getopt_long(argc, argv,
+                      short_options, long_options, &idx);
+
+        if (-1 == c)
+            break;
+
+        switch (c) {
+            case 0: /* getopt_long() flag */
+                break;
+
+            case 'l':
+                ffmpeg_log = 1;
+                break;
+
+            case 'd':
+                g_dw_mode = atoi(optarg);
+                if (g_dw_mode != 0 &&
+                        g_dw_mode != 1 &&
+                        g_dw_mode != 2 &&
+                        g_dw_mode != 4 &&
+                        g_dw_mode != 16) {
+                    printf("invalide dw_mode %d\n", g_dw_mode);
+                    exit(1);
+                }
+                break;
+
+            case 'f':
+                media_file = strdup(optarg);
+                break;
+
+            case 'h':
+                usage(argc, argv);
+                return -1;
+
+            case 'p':
+                global_plane_id = atoi(optarg);
+                break;
+
+            default:
+                usage(argc, argv);
+                return -1;
+        }
+    }
+    return 0;
+}
+
+static void sig_handler(int signo)
+{
+    dump_v4l2_decode_state();
+    decode_finish();
+}
+
+int main(int argc, char *argv[])
+{
+    int rc;
+
+    if (parse_para(argc, argv))
+        goto error;
+
+    if (!media_file) {
+        printf("no file name assigned\n");
+        goto error;
+    }
+
+    sem_init(&wait_for_end, 0, 0);
+
+    display_engine_register_cb(capture_buffer_recycle);
+    rc = display_engine_start();
+    if (rc < 0) {
+        printf("Unable to start display engine\n");
+        goto error;
+    }
+    printf("\ndisplay started\n");
+
+    struct dmx_cb cb = {
+        .write = v4l2_dec_write_es,
+        .frame_done = v4l2_dec_frame_done,
+        .meta_done = start_decoder,
+        .eos = v4l2_dec_eos,
+    };
+
+    signal(SIGINT, sig_handler);
+
+    rc = demux_init(media_file, &cb);
+    if (rc) {
+        printf("demux_init fail %d\n",rc);
+        goto error;
+    }
+    printf("\ndemux started\n");
+
+    sem_wait(&wait_for_end);
+
+    dmx_destroy();
+    v4l2_dec_destroy();
+    display_engine_stop();
+    sem_destroy(&wait_for_end);
+
+    return 0;
+error:
+	return 1;
+}
---
+++ v4l2-uvm-test/v4l2-uvm-test.mk
@@ -0,0 +1,20 @@
+#
+# v4l2-uvm-test
+#
+V4L2_UVM_TEST_VERSION = 0.1
+V4L2_UVM_TEST_SITE = $(TOPDIR)/../multimedia/v4l2-uvm-test/src
+V4L2_UVM_TEST_SITE_METHOD = local
+
+define V4L2_UVM_TEST_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC=$(TARGET_CC) -C $(@D)/
+endef
+
+define V4L2_UVM_TEST_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC=$(TARGET_CC) -C $(@D)/ install
+endef
+
+define V4L2_UVM_TEST_INSTALL_CLEAN_CMDS
+    $(MAKE) CC=$(TARGET_CC) -C $(@D) clean
+endef
+
+$(eval $(generic-package))
